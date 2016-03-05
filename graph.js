ef06125bdf4efd616a7f47dcdc0e6b01d1609a37
Barrie Treloar <baerrach@gmail.com>
2016-03-06 06:37:20 +1030

    Add links

--- graph.js
+++ graph.js
@@ -1,5 +1,19 @@
 /*jshint esnext: true */
 
+// Polyfill
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
+if (!String.prototype.endsWith) {
+  String.prototype.endsWith = function(searchString, position) {
+      var subjectString = this.toString();
+      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+        position = subjectString.length;
+      }
+      position -= searchString.length;
+      var lastIndex = subjectString.indexOf(searchString, position);
+      return lastIndex !== -1 && lastIndex === position;
+  };
+}
+
 var featsElement = d3.select("#feats");
 var width = featsElement.style("width").replace("px", "");
 var height = window.innerHeight -4 -4; // 4px padding top and bottom
@@ -23,7 +37,12 @@ var feats = {
   links: [],
   cache: {}
 };
-feats.addLinks = function () {
+feats.addLinks = function (feat) {
+  var prerequisitesAsLinks = this.getPrerequisitesAsLinks(feat);
+
+  prerequisitesAsLinks.forEach(function(prerequisite) {
+    this.links.push(prerequisite)
+  }, this);
 }
 feats.addNode = function(value) {
   var node = this.getNode([value.name]);
@@ -31,6 +50,59 @@ feats.addNode = function(value) {
 }
 feats.createNode = function(name) {
   return {};
+}
+feats.getPrerequisitesAsLinks = function(feat) {
+  var prerequisitesAsLinks = [];
+  var prerequisitesAsString = feat.prerequisites;
+  var andPrerequisites;
+
+  if (prerequisitesAsString.endsWith(".")) {
+    prerequisitesAsString = prerequisitesAsString.slice(0, -1);
+  }
+  if (prerequisitesAsString.indexOf(";") !== -1) {
+    andPrerequisites = prerequisitesAsString.split(";");
+    andPrerequisites.forEach(function (andPrerequisite) {
+      if (andPrerequisite.indexOf(",") !== -1) {
+        var orPrerequisites = andPrerequisite.split(",");
+        orPrerequisites.forEach(function (orPrerequisite) {
+          prerequisitesAsLinks.push(
+            {
+              source: this.getNode(feat.name),
+              target: this.getNode(orPrerequisite),
+              type: "OR",
+              description: andPrerequisite
+            }
+          );
+        }, this);
+      }
+      else {
+        prerequisitesAsLinks.push(
+          {
+            source: this.getNode(feat.name),
+            target: this.getNode(andPrerequisite),
+            type: "AND",
+            description: andPrerequisite
+          }
+        );
+      }
+    }, this);
+  }
+  else {
+    andPrerequisites = prerequisitesAsString.split(",");
+    andPrerequisites.forEach(function (prerequisite) {
+      prerequisitesAsLinks.push(
+        {
+          source: this.getNode(feat.name),
+          target: this.getNode(prerequisite),
+          type: "AND",
+          description: prerequisite
+        }
+      );
+    }, this);
+  }
+
+
+  return prerequisitesAsLinks;
 }
 feats.getNode = function(name) {
   if (this.cache[name] === undefined) {
@@ -56,21 +128,21 @@ feats.loadNodes = function(feats) {
 }
 
 function renderFeats() {
-  console.log("renderFeats");
   force
     .nodes(feats.nodes)
     .links(feats.links)
     .on("tick", tick)
     .start();
 
-  link = link.data(feats.links)
-    .enter().append("line")
-    .attr("class", "link");
-
   node = node.data(feats.nodes)
     .enter().append("circle")
     .attr("class", "node")
     .attr("r", 12);
+
+  link = link.data(feats.links)
+    .enter().append("line")
+    .attr("class", "link");
+
 
   //  FeatsElement.style("height", height + "px");
 }
@@ -108,4 +180,3 @@ Promise.all(promisedCsvs)
 
     update();
   });
-
