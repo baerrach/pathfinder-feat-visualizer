e09eb595c577ccd61c36eec1e0ced9617583c3ee
Barrie Treloar <baerrach@gmail.com>
2016-03-09 21:57:37 +1030

    Layout graph with text, and link arrows.
    New csv file from pathfinder community, has some duplicates that need
    handling.
    
    Link stroke and arrow are slightly opaque.
    
    Increase zoom to 8.
    
    Trying some force parameters to speed up simulation times.
    More work needs to be done there.
    
    Node creation needs an initial x, y as these were throwing NaN instead.
    This may have been because some values were set and not all.
    Setting everything to be at 0,0 is an issue for the simulation as the
    charge factor flings things too far and having NaN, -Infinity, or
    +Infinity isn't good for the simulation. Instead they are initialized as
    a grid, but once a decent layout is achieved the current x,y values need
    to be captured and used to seed the simulation.
    
    Mythic feats have the same name, but a different category. To avoid
    these being duplicates the name is changed to include a (Mythic) suffix.
    
    Draw order is important for svg, what is drawn first is lower in the
    z-order and hence will be over-drawn with higher order values. So links
    need to get drawn first so that the node will draw over the top of the
    line end points. The proper way to fix that would be to adjust the line
    length to be shorter on each end by the node's radius, but that is more
    maths than necessary. The current hack just moves the arrow along the
    x-axis so that it pokes out near the node circumference.
    
    Nodes are also an svg g element, that contains the circle and the text.
    This means that translate() is needed to move them around instead of
    adjusting the circles point of origin.

--- graph.js
+++ graph.js
@@ -4,16 +4,17 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/endsWith
 if (!String.prototype.endsWith) {
   String.prototype.endsWith = function(searchString, position) {
-      var subjectString = this.toString();
-      if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
-        position = subjectString.length;
-      }
-      position -= searchString.length;
-      var lastIndex = subjectString.indexOf(searchString, position);
-      return lastIndex !== -1 && lastIndex === position;
+    var subjectString = this.toString();
+    if (typeof position !== 'number' || !isFinite(position) || Math.floor(position) !== position || position > subjectString.length) {
+      position = subjectString.length;
+    }
+    position -= searchString.length;
+    var lastIndex = subjectString.indexOf(searchString, position);
+    return lastIndex !== -1 && lastIndex === position;
   };
 }
 
+var radius = 12;
 var featsElement = d3.select("#feats");
 var width = featsElement.style("width").replace("px", "");
 var height = window.innerHeight -4 -4; // 4px padding top and bottom
@@ -23,20 +24,21 @@ var svg = featsElement.append("svg")
 
 // build the arrow (http://bl.ocks.org/d3noob/5141278)
 svg.append("svg:defs").selectAll("marker")
-    .data(["end"])      // Different link/path types can be defined here
+  .data(["end"])      // Different link/path types can be defined here
   .enter().append("svg:marker")    // This section adds in the arrows
-    .attr("id", String)
-    .attr("viewBox", "0 -5 10 10")
-    .attr("refX", 15)
-    .attr("refY", -1.5)
-    .attr("markerWidth", 6)
-    .attr("markerHeight", 6)
-    .attr("orient", "auto")
+  .attr("id", String)
+  .attr("class", "arrow")
+  .attr("viewBox", "0 -5 10 10")
+  .attr("refX", 25)
+  .attr("refY", 0)
+  .attr("markerWidth", 6)
+  .attr("markerHeight", 6)
+  .attr("orient", "auto")
   .append("svg:path")
-    .attr("d", "M0,-5L10,0L0,5");
+  .attr("d", "M0,-5L10,0L0,5");
 
 svg = svg.append("g")
-  .call(d3.behavior.zoom().scaleExtent([0.25, 4]).on("zoom", zoom))
+  .call(d3.behavior.zoom().scaleExtent([0.125, 8]).on("zoom", zoom))
 
 svg.append("rect")
   .attr("class", "overlay")
@@ -50,11 +52,12 @@ var link = svg.selectAll(".link"),
 
 var force = d3.layout.force()
       .size([width, height])
-      .linkDistance(60)
-      .charge(-300);
+      .theta(0.95)
+      .linkDistance(30)
+      .charge(-350);
 
 /* Initialize tooltip */
-tip = d3.tip().attr('class', 'd3-tip').html(function(d) { 
+tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
   var tipText = "No name";
   if (d.value) {
     tipText = d.value.name;
@@ -64,7 +67,7 @@ tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
 svg.call(tip);
 
 var csvs = [
-  "feats.csv",
+  "Feats - Updated 01Mar2016.csv",
 ];
 var promisedCsvs = csvs.map(csv);
 
@@ -81,16 +84,29 @@ feats.addLinks = function (feat) {
   }, this);
 }
 feats.addNode = function(value) {
-  var node = this.getNode([value.name]);
+  var node = this.getNode(value.name);
   node.value = value;
-}
-feats.createNode = function(name) {
-  return { value: { name: name} };
+  node.x = Number(value.x || 0);
+  node.y = Number(value.y || 0);
+  node.fixed = false;
 }
 feats.getPrerequisitesAsLinks = function(feat) {
   var prerequisitesAsLinks = [];
   var prerequisitesAsString = feat.prerequisites;
   var andPrerequisites;
+
+  if (!prerequisitesAsString) {
+    return [];
+  }
+
+  // Small, Tiny, Diminutive, or Fine.
+  // Large, Huge, Gargantuan, Colossal.
+
+  // <skill> X rank re-link back to basic skill
+  // <attribute> X re-link back to basic attribute
+  // same for BAB
+  // same for caster level?
+  // same for class level X
 
   if (prerequisitesAsString.endsWith(".")) {
     prerequisitesAsString = prerequisitesAsString.slice(0, -1);
@@ -98,9 +114,16 @@ feats.getPrerequisitesAsLinks = function(feat) {
   if (prerequisitesAsString.indexOf(";") !== -1) {
     andPrerequisites = prerequisitesAsString.split(";");
     andPrerequisites.forEach(function (andPrerequisite) {
-      if (andPrerequisite.indexOf(",") !== -1) {
-        var orPrerequisites = andPrerequisite.split(",");
+      if (!andPrerequisite) {
+        return;
+      }
+
+      if (andPrerequisite.indexOf(", ") !== -1) {
+        var orPrerequisites = andPrerequisite.split(", ");
         orPrerequisites.forEach(function (orPrerequisite) {
+          if (orPrerequisite.startsWith("or ")) {
+            orPrerequisite = orPrerequisite.slice(2);
+          }
           prerequisitesAsLinks.push(
             {
               source: this.getNode(feat.name),
@@ -124,7 +147,7 @@ feats.getPrerequisitesAsLinks = function(feat) {
     }, this);
   }
   else {
-    andPrerequisites = prerequisitesAsString.split(",");
+    andPrerequisites = prerequisitesAsString.split(", ");
     andPrerequisites.forEach(function (prerequisite) {
       prerequisitesAsLinks.push(
         {
@@ -141,8 +164,19 @@ feats.getPrerequisitesAsLinks = function(feat) {
   return prerequisitesAsLinks;
 }
 feats.getNode = function(name) {
+  function createNode(name) {
+    return {
+      x: 0,
+      y: 0,
+      value: { name: name }
+    };
+  }
+
+  name = name.trim();
+  name = name.charAt(0).toUpperCase() + name.slice(1);
+
   if (this.cache[name] === undefined) {
-    this.cache[name] = this.createNode(name);
+    this.cache[name] = createNode(name);
   }
   return this.cache[name];
 }
@@ -151,16 +185,36 @@ feats.loadNodes = function(feats) {
   this.links = [];
   this.cache = {};
 
-  var feat = null;
+  // Add the nodes
   feats.forEach(function (feat) {
+    if ("Mythic" === feat.type) {
+      feat.name = feat.name + " (Mythic)";
+    }
+    if (this.cache[feat.name]) {
+      feat.name = feat.name + " (Duplicate)"
+      feat.prerequisites = "Duplicate";
+    }
     this.addNode(feat);
+  }, this);
+
+  // Do the links last to avoid creating duplicates nodes
+  // when the prequisites creates placeholders instead of real nodes.
+  // This way the only place holders are for non-feat prerequisites.
+  feats.forEach(function (feat) {
     this.addLinks(feat);
   }, this);
+
 
   Object.keys(this.cache).forEach(function (name) {
     var node = this.cache[name];
     this.nodes.push(node);
   }, this);
+
+  var n = Math.floor(Math.sqrt(this.nodes.length));
+  this.nodes.forEach(function(d, i) {
+    d.x = width/2 + 10*(i % n);
+    d.y = height/2 + 10*(Math.floor(i / n));
+  });
 }
 
 function renderFeats() {
@@ -168,32 +222,44 @@ function renderFeats() {
     .nodes(feats.nodes)
     .links(feats.links)
     .on("tick", tick)
-    .start();
-
-  node = node.data(feats.nodes)
-    .enter().append("circle")
-    .attr("class", "node")
-    .attr("r", 12)
-    .on('mouseover.tip', tip.show)
-    .on('mouseout.tip', tip.hide);
 
   link = link.data(feats.links)
     .enter().append("line")
     .attr("class", "link")
     .attr("marker-end", "url(#end)");;
 
+  node = node.data(feats.nodes)
+    .enter().append("g")
+    .attr("class", "node");
 
-  //  FeatsElement.style("height", height + "px");
+  node.append("circle")
+    .attr("class", "node")
+    .attr("r", radius)
+    .on('mouseover.tip', tip.show)
+    .on('mouseout.tip', tip.hide);
+
+  node.append("text")
+    .attr("x", radius + 4)
+    .attr("dy", ".35em")
+    .text(function(d) {
+      return d.value.name;
+    });
+
+  force.start();
 }
 
 function tick() {
-  link.attr("x1", function(d) { return d.source.x; })
+
+  link.attr("x1", function(d) {
+    return d.source.x;
+  })
     .attr("y1", function(d) { return d.source.y; })
     .attr("x2", function(d) { return d.target.x; })
     .attr("y2", function(d) { return d.target.y; });
 
-  node.attr("cx", function(d) { return d.x; })
-    .attr("cy", function(d) { return d.y; });
+  node
+    .attr("transform", function(d) {
+      return "translate(" + d.x + "," + d.y + ")"; });
 }
 
 function csv(url) {
@@ -223,3 +289,30 @@ Promise.all(promisedCsvs)
 
     update();
   });
+
+function printXYFeats() {
+  console.log( "id, name, x, y" );
+  feats.nodes.forEach(function (node) {
+    if (node.value.id) {
+      console.log( node.value.id + "," + node.value.name + ", " + node.x + "," + node.y );
+    }
+  });
+}
+
+function printXYPrequisites() {
+  console.log( "name, x, y" );
+  feats.nodes.forEach(function (node) {
+    if (!node.value.id) {
+      console.log( node.value.name + "," + node.x + "," + node.y );
+    }
+  });
+}
+
+function printXYAllNodes() {
+  console.log( "id, name, x, y" );
+  feats.nodes.forEach(function (node) {
+    var id = node.value.id || 0;
+    console.log( id + "," + node.value.name + ", " + node.x + "," + node.y );
+  });
+}
+
